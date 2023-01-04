fda792742a35322c2f9626a3e8d61e652166aefe
han-tyumi <mmchamp95@gmail.com>
2023-01-04 13:26:49 -0500

    feat: improve base command

--- src/index.ts
+++ src/index.ts
@@ -9,27 +9,47 @@ import soundtrap from './commands/soundtrap.js'
 import cache from './commands/cache/index.js'
 import export_ from './commands/export/index.js'
 
+let killed = false
+const setKilled = () => {
+  killed = true
+}
+
+process.on('SIGINT', setKilled)
+process.on('SIGQUIT', setKilled)
+process.on('SIGTERM', setKilled)
+
 $.verbose = false
 
 const argv = yargs(process.argv.slice(2))
   .scriptName('ut')
   .env('UT')
-  .completion('completion', 'Generate completion script')
-  .recommendCommands()
-  .version()
-  .help()
+  .fail(false)
   .command(bandlab)
   .command(soundtrap)
   .command(cache)
   .command(export_)
+  .completion('completion', 'Generate completion script')
+  .recommendCommands()
+  .version()
+  .help()
+  .alias({
+    h: 'help',
+    v: 'version',
+  })
 
 const explorer = cosmiconfigSync('ut')
 const searchResult = explorer.search()
 
-await (searchResult?.config
-  ? argv.config(searchResult.config)
-  : argv.config('C', 'Path to config file', (configPath) => {
-      const result = explorer.load(configPath)
-      return result?.config as Record<string, unknown>
-    })
-).parse()
+try {
+  await (searchResult?.config
+    ? argv.config(searchResult.config)
+    : argv.config('C', 'Path to config file', (configPath) => {
+        const result = explorer.load(configPath)
+        return result?.config as Record<string, unknown>
+      })
+  ).parse()
+} catch (error: unknown) {
+  if (!killed) {
+    throw error
+  }
+}
